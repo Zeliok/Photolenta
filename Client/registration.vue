e77fd8ae02b749876397d5db777288140831b56f
Zeliok <53336570+Zeliok@users.noreply.github.com>
2019-08-23 13:44:37 +0300

    appdata vue

---
+++ Client/registration.vue
@@ -0,0 +1,28 @@
+<style>
+	#button{
+	background-color: red;
+	color: white;
+	border: solid 1px red;
+	width: 700px;
+	}
+	form{
+	width: 700px;
+	}	
+</style>
+
+<template>
+	<div>
+		<form>
+			  <div class="form-group">
+			    <input type="email" class="form-control" id="login" aria-describedby="emailHelp" placeholder="Логин">
+			  </div>
+			  <div class="form-group">
+			    <input type="password" class="form-control" id="password" placeholder="Пароль">
+			  </div>
+			  <div class="form-group">
+			  	<input type="password" class="form-control" id="Repeat password" placeholder="Повтор пароля">
+			  </div>
+			  <button type="submit" class="btn btn-primary" id="button">Зарегистрироваться!</button>
+			</form>
+	</div>
+</template>
